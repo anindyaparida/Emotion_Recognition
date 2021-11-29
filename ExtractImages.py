eddb92dee70563ed9614c0d7b283aef1dd46f23a
anindyaparida <parida.2@iitj.ac.in>
2021-11-30 00:22:50 +0530

    Code change - to modify image name

--- ExtractImages.py
+++ ExtractImages.py
@@ -5,81 +5,69 @@ Created on Sat Oct  9 22:41:13 2021
 @author: pallavi
 """
 
-# Importing all necessary libraries
+# Importing  necessary libraries
 import cv2
 import os
 from os import walk
-from duplicateRem import DuplicateRemover
 
-mypath=r"C:\Users\anind\Downloads\MTP\Videos"
+mypath = r"C:\Users\anind\Downloads\MTP\Videos"
 
 f = []
 for (dirpath, dirnames, filenames) in walk(mypath):
-  f.extend(filenames)
-  break
-# printing file names
-print(f)  
-
-videoNumber=0
+    f.extend(filenames)
+    break
+# printing file names in the video folder
+print(f)
 
 for videoFile in f:
-  # Read the video from specified path
-  cam = cv2.VideoCapture(mypath+"\\"+videoFile)
-  videoNumber=videoNumber+1  
-  try:
-        
-      # creating a folder named data
-      if not os.path.exists('images'):
-          os.makedirs('images')
-    
-  # if not created then raise error
-  except OSError:
-      print ('Error: Creating directory of data')
-      
-  fps = 25
-  
-  (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
-  # With webcam get(CV_CAP_PROP_FPS) does not work.
-  # Let's see for ourselves.
-  if int(major_ver)  < 3 :
-      fps = cam.get(cv2.cv.CV_CAP_PROP_FPS)
-      print("Frames per second for video is: {0}".format(fps))
-  else :
-      fps = cam.get(cv2.CAP_PROP_FPS)
-      print("Frames per second for video is: {0}".format(fps))
-      
-  # frame
-  currentframe = 0
-  count = 0
-  
-  while(True):
-        
-      # reading from frame
-      ret,frame = cam.read()
-    
-      if ret:
-          count +=1
-          if count%(fps*20) == 0:
-              # if video is still left continue creating images
-              name = './images/videoNumber'+str(videoNumber)+'frame' + str(currentframe) + '.jpg'
-              #print ('Cr eating...' + name)
-        
-              # writing the extracted images
-              cv2.imwrite(name, frame)
+    # Read the video from the specified path
+    cam = cv2.VideoCapture(mypath + "\\" + videoFile)
+    videoFileName = videoFile[:-4]
+    try:
 
-              # increasing counter so that it will
-              # show how many frames are created
-              currentframe += 1
+        # creating a folder named images for storing extracting images
+        if not os.path.exists('images'):
+            os.makedirs('images')
 
-      else:
-          break
+    # if not created then raise error
+    except OSError:
+        print('Error: Creating directory of data')
 
-  dirname = r"C:\Users\anind\Downloads\MTP\images"
+    fps = 25
+    # Get value of FPS for the video
+    (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+    # With webcam get(CV_CAP_PROP_FPS) does not work.
+    # Let's see for ourselves.
+    if int(major_ver) < 3:
+        fps = cam.get(cv2.cv.CV_CAP_PROP_FPS)
+        print("Frames per second for video is: {0}".format(fps))
+    else:
+        fps = cam.get(cv2.CAP_PROP_FPS)
+        print("Frames per second for video is: {0}".format(fps))
 
-  # Remove Duplicates
-  dr = DuplicateRemover(dirname)
-  dr.find_duplicates()
-    
-  # Release all space and windows once done
-  cam.release()
-  cv2.destroyAllWindows()
+    # frame
+    currentframe = 0
+    count = 0
+
+    while (True):
+
+        # reading from frame
+        ret, frame = cam.read()
+
+        if ret:
+            count += 1
+            if count % (fps * 10) == 0:
+                # name of extracted image frame from the video
+                name = './images/' + str(videoFileName) + '_frame' + str(currentframe) + '.jpg'
+
+                # writing the extracted images
+                cv2.imwrite(name, frame)
+
+                # increasing counter for the number of frames being created
+                currentframe += 1
+        else:
+            break
+
+    # Release all space and windows once done
+    cam.release()
+    cv2.destroyAllWindows()
